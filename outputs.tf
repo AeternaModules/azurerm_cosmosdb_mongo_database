c0c4fd4d1772cf01aeda548c9586693818ef29ed
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "cosmosdb_mongo_databases" {
-  description = "All cosmosdb_mongo_database resources"
-  value       = azurerm_cosmosdb_mongo_database.cosmosdb_mongo_databases
-}
 output "cosmosdb_mongo_databases_account_name" {
-  description = "List of account_name values across all cosmosdb_mongo_databases"
-  value       = [for k, v in azurerm_cosmosdb_mongo_database.cosmosdb_mongo_databases : v.account_name]
+  description = "Map of account_name values across all cosmosdb_mongo_databases, keyed the same as var.cosmosdb_mongo_databases"
+  value       = { for k, v in azurerm_cosmosdb_mongo_database.cosmosdb_mongo_databases : k => v.account_name }
 }
 output "cosmosdb_mongo_databases_autoscale_settings" {
-  description = "List of autoscale_settings values across all cosmosdb_mongo_databases"
-  value       = [for k, v in azurerm_cosmosdb_mongo_database.cosmosdb_mongo_databases : v.autoscale_settings]
+  description = "Map of autoscale_settings values across all cosmosdb_mongo_databases, keyed the same as var.cosmosdb_mongo_databases"
+  value       = { for k, v in azurerm_cosmosdb_mongo_database.cosmosdb_mongo_databases : k => v.autoscale_settings }
 }
 output "cosmosdb_mongo_databases_name" {
-  description = "List of name values across all cosmosdb_mongo_databases"
-  value       = [for k, v in azurerm_cosmosdb_mongo_database.cosmosdb_mongo_databases : v.name]
+  description = "Map of name values across all cosmosdb_mongo_databases, keyed the same as var.cosmosdb_mongo_databases"
+  value       = { for k, v in azurerm_cosmosdb_mongo_database.cosmosdb_mongo_databases : k => v.name }
 }
 output "cosmosdb_mongo_databases_resource_group_name" {
-  description = "List of resource_group_name values across all cosmosdb_mongo_databases"
-  value       = [for k, v in azurerm_cosmosdb_mongo_database.cosmosdb_mongo_databases : v.resource_group_name]
+  description = "Map of resource_group_name values across all cosmosdb_mongo_databases, keyed the same as var.cosmosdb_mongo_databases"
+  value       = { for k, v in azurerm_cosmosdb_mongo_database.cosmosdb_mongo_databases : k => v.resource_group_name }
 }
 output "cosmosdb_mongo_databases_throughput" {
-  description = "List of throughput values across all cosmosdb_mongo_databases"
-  value       = [for k, v in azurerm_cosmosdb_mongo_database.cosmosdb_mongo_databases : v.throughput]
+  description = "Map of throughput values across all cosmosdb_mongo_databases, keyed the same as var.cosmosdb_mongo_databases"
+  value       = { for k, v in azurerm_cosmosdb_mongo_database.cosmosdb_mongo_databases : k => v.throughput }
 }
 
