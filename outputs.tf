8c9c3628dd2ea1bbbf3b0a7d8289dcf3629562d6
Daniel Allen <dan@aeternagroup.net>
2026-08-15 00:51:46 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,7 @@ output "cosmosdb_mongo_databases_account_name" {
 }
 output "cosmosdb_mongo_databases_autoscale_settings" {
   description = "Map of autoscale_settings values across all cosmosdb_mongo_databases, keyed the same as var.cosmosdb_mongo_databases"
-  value       = { for k, v in azurerm_cosmosdb_mongo_database.cosmosdb_mongo_databases : k => v.autoscale_settings if v.autoscale_settings != null && length(v.autoscale_settings) > 0 }
+  value       = { for k, v in azurerm_cosmosdb_mongo_database.cosmosdb_mongo_databases : k => one(v.autoscale_settings) if v.autoscale_settings != null && length(v.autoscale_settings) > 0 }
 }
 output "cosmosdb_mongo_databases_name" {
   description = "Map of name values across all cosmosdb_mongo_databases, keyed the same as var.cosmosdb_mongo_databases"
